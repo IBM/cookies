cba79fc888081831a397818fadd26f9d13d06158
Ryan Nelson <Ryan.Nelson@ibm.com@ryans-macbook-pro-2.local>
2020-11-18 09:15:04 -0500

    added library

---
+++ index.js
@@ -0,0 +1,32 @@
+export default {
+  set: function(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie =
+      encodeURIComponent(name) +
+      "=" +
+      (encodeURIComponent(value) || "") +
+      expires +
+      "; path=/";
+  },
+
+  get: function getCookie(name) {
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0)
+        return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+  },
+
+  erase: function(name) {
+    document.cookie = encodeURIComponent(name) + "=; Max-Age=-99999999;";
+  }
+};
